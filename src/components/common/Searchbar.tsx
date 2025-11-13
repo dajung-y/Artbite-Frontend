42233d6c4696dc0f824c85b37a6802b3a0303770
dajung-y <0352wjd@naver.com>
2025-11-13 13:56:17 +0900

    style: 서치바 스타일 적용

--- src/components/common/Searchbar.tsx
+++ src/components/common/Searchbar.tsx
@@ -1,5 +1,5 @@
 // src/components/commom/Searchbar.tsx
-// 서치바 : HistoryPage, BookmarkPage 
+// 서치바 : HistoryPage, BookmarkPage 스타일 적용 완료
 
 import { useState } from "react"
 import { FiSearch } from "react-icons/fi";
@@ -9,10 +9,10 @@ export default function Searchbar() {
 
   return (
     <div className="px-5 w-full">
-      <div className="self-stretch w-full px-4 py-2 bg-gray-500 rounded-lg inline-flex items-center gap-1">
+      <div className="self-stretch w-full px-4 py-2 bg-greyscale-600 rounded-lg inline-flex items-center gap-1">
         {/* 검색 아이콘 */}
-        <div className="w-6 h-6 overflow-hidden">
-          <FiSearch className="w-full h-full text-gray-300" />
+        <div className="w-4 h-4 overflow-hidden">
+          <FiSearch className="w-full h-full text-greyscale-400" />
         </div>
         {/* 검색 입력 */}
         <input
@@ -20,7 +20,7 @@ export default function Searchbar() {
           value={searchValue}
           onChange={e => setSearchValue(e.target.value)}
           placeholder="작업노트, 작가 이름"
-          className="w-full bg-transparent text-base font-light text-gray-300 focus:outline-none"
+          className="w-full bg-transparent placeholder-greyscale-400 text-body text-greyscale-100 focus:outline-none"
           />
       </div>
     </div>
